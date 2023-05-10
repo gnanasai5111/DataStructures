6c06215021c736e4a1f6daadafec999c2ac2591c
gnanasai5111 <56872239+gnanasai5111@users.noreply.github.com>
2023-05-10 20:16:41 +0530

    Update isomorphicStrings.java

--- Hashing/isomorphicStrings.java
+++ Hashing/isomorphicStrings.java
@@ -80,3 +80,31 @@ class Solution {
    
 Time complexity - o(N)
 Space complexity - o(N)
+    
+Fourth approach(Hashing)
+    
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        HashMap<Character,Character> map=new HashMap<>();
+        for(int i=0;i<s.length();i++){
+           if(map.containsKey(s.charAt(i))){
+             if(map.get(s.charAt(i))!=t.charAt(i)){
+                 return false;
+             }
+           }
+           else{
+                if(map.containsValue(t.charAt(i))){
+                    return false;
+                }
+                map.put(s.charAt(i),t.charAt(i)); 
+           }
+ 
+        }
+        return true;
+    }
+}
+
+1. Map the characters in hashmap.Check if the mapped character is equal to value at current index and also check if mapped value is already present as value.
+    
+Time complexity - o(N)
+Space complexity - o(N)
